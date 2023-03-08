fd6218ba2d085c9bf2af58dec3f6516e9e11fce8
tanke <jtanke@uoregon.edu>
2023-03-07 20:23:42 -0800

    clean up

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/tanke/launchflow/buildflow-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/tanke/launchflow/buildflow-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/tanke/launchflow/buildflow-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/tanke/launchflow/buildflow-docs/src/css/custom.css'),
-];
