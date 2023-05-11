7a8dca01a2382fe61c298d9da8fc3da6b1be8283
tanke <jtanke@uoregon.edu>
2023-05-10 19:53:46 -0700

    disable blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,12 +57,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/launchflow/buildflow-docs/tree/main/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/launchflow/buildflow-docs/tree/main/",
-        },
+        blog: false,
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
